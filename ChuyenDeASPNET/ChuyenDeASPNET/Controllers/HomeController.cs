abf04a1221f09ac1ac9edda0922324b5761713c0
VoThiHuynhPhuong <vohuynhphuong02250405@gmail.com>
2024-12-18 19:00:25 +0700

    Resolve conflict

---
+++ ChuyenDeASPNET/ChuyenDeASPNET/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using ChuyenDeASPNET.Context;
+using ChuyenDeASPNET.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ChuyenDeASPNET.Controllers
+{
+    public class HomeController : Controller
+    {
+        HuynhPhuongASPNETEntities1 objHuynhPhuongASPNETEntities = new HuynhPhuongASPNETEntities1();
+        public ActionResult Index()
+        {
+            HomeModel objHomeModel = new HomeModel();
+            objHomeModel.ListProduct = objHuynhPhuongASPNETEntities.Product.ToList();
+            objHomeModel.ListCategories = objHuynhPhuongASPNETEntities.Categories.ToList();
+
+            return View(objHomeModel);
+        }
+    }
+}
